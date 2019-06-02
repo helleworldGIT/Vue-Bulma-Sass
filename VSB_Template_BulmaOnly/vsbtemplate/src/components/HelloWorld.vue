be3312b7281f4bf59aadcf069de1a6fe8f220c36
Desiré <44606766+helleworldGIT@users.noreply.github.com>
2019-06-02 18:28:58 +0200

    Template - Only Bulma
    Bulma integrated in a Vue project.

---
+++ VSB_Template_BulmaOnly/vsbtemplate/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- BULMA TEST -->
+    <div class="columns">
+      <div class="column is-12">
+        <h1 class="has-text-info title is-1">
+          This is a Bulma test!
+        </h1>
+        <h4 class="has-text-info subtitle has-text-success">
+          Hello, this is Bulma: I'm easier than Bootstrap!
+        </h4>
+          <button class="button is-primary">
+            A primary button.
+          </button>
+          <button class="button is-link">
+            A link button.
+          </button>
+          <button class="button is-danger">
+            A danger button.
+          </button>
+      </div>
+    </div>
+    <!-- /BULMA TEST -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+/** Importing our Bulma css file **/
+@import url("../css/main.css");
+</style>
